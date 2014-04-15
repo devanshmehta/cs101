4697f516361379e2c6c489fc4b7f0c78041bad16
Devansh Mehta <devansh.mht@gmail.com>
2014-04-15 19:15:40 -0400

    operations to be performed on the heap

--- heap.py
+++ heap.py
@@ -1,1 +1,28 @@
+"""Contains the heap data structure and 
+    algorithm on that data structure."""
+    
 __author__ = 'devansh.mht@gmail.com'
+
+class Heap(object):
+  
+  """Heap which consists of numbers"""
+  
+  def __init__(self):
+    self.__heap = []
+  
+  def max_heapify(self):
+    pass
+  
+  def extract_max(self):
+    """extracts the max element from the heap"""
+    pass
+    
+  def insert(self, i):
+    """inserts element i in the heap"""
+    pass
+  
+  def peek(self):
+    """returns the current max element of the heap. 
+       This method does not extract the max element"""
+       pass
+  
