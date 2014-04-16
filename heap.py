f9e65537dec2cb7cdd58253e2778881aa1ead6f5
Devansh Mehta <devansh.mht@gmail.com>
2014-04-16 08:50:48 -0400

    implementation for insert, peak and sift_up

--- heap.py
+++ heap.py
@@ -7,16 +7,17 @@ class Heap(object):
   
   """Heap which consists of numbers"""
   
-  def __init__(self):
-    self.__heap = []
+  def __init__(self, heap = []):
+    self.__heap = heap
     
   def build_heap(self):
       """builds the heap"""
       pass
   
-  def max_heapify(self, i):
+  def max_heapify(self, i, max_size = 0):
     """assumes both the left and right subtree 
-       are max heapify"""
+       are max heapify. Max size is zero if we want to 
+       operate on the entire heap"""
     if i < 1 or i >= len(self.__heap):
         return
     left_child = 2 * i
@@ -35,13 +36,23 @@ class Heap(object):
   def extract_max(self):
     """extracts the max element from the heap"""
     pass
-    
+
+  def sift_up(self, index):
+    parent_index = index // 2
+    if self.__heap[parent_index] < self.__heap[index]:
+      temp = self.__heap[parent_index]
+      self.__heap[parent_index] = self.__heap[index]
+      self.__heap[index] = temp
+      self.sift_up(parent_index)
+
   def insert(self, i):
     """inserts element i in the heap"""
-    pass
-  
+    self.__heap.append(i)
+    index = len(self.__heap) - 1
+    self.sift_up(index)
+    
   def peek(self):
     """returns the current max element of the heap. 
        This method does not extract the max element"""
-       pass
+    return self.__heap[1]
   
