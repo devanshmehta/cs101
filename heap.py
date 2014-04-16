1c500e739e792a8044102c905eb2ce0e8ce3b670
Devansh Mehta <devansh.mht@gmail.com>
2014-04-15 23:29:33 -0400

    updating max_heapify method

--- heap.py
+++ heap.py
@@ -9,13 +9,29 @@ class Heap(object):
   
   def __init__(self):
     self.__heap = []
+    
+  def build_heap(self):
+      """builds the heap"""
+      pass
   
   def max_heapify(self, i):
     """assumes both the left and right subtree 
        are max heapify"""
     if i < 1 or i >= len(self.__heap):
         return
-  
+    left_child = 2 * i
+    right_child = (2 * i) + 1
+    largest = i
+    if self.__heap[left_child] > self.__heap[largest]:
+        largest = left_child
+    if self.__heap[right_child] > self.__heal[largest]:
+        largest = right_child
+    if largest != i:
+        temp = self.__heap[i]
+        self.__heap[i] = self.__heap[largest]
+        self.__heap[largest] = temp
+        max_heapify(largest)
+
   def extract_max(self):
     """extracts the max element from the heap"""
     pass
