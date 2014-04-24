6f1520ce4f6fbc233ffd6c4966f7721d6fba15ce
Devansh Mehta <devansh.mht@gmail.com>
2014-04-23 22:42:37 -0400

    methods in avl tree

--- avl.py
+++ avl.py
@@ -3,3 +3,27 @@
 __author__ = 'devansh.mht@gmail.com'
 
 class AvlTree:
+  
+  def __init__(self):
+    pass
+  
+  def insert(self, i):
+    pass
+  
+  def delete(self, i):
+    pass
+  
+  def find_min(self):
+    pass
+  
+  def find_max(self):
+    pass
+  
+  def successor(self):
+    pass
+  
+  def predecessor(self):
+    pass
+  
+  def sort_tree(self):
+    pass
