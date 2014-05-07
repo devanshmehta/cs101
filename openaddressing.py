897c10b55f8b6fb4878c4dd4d4d2428947027d4c
Devansh Mehta <devansh.mht@gmail.com>
2014-05-06 22:31:57 -0400

    implementation for insert

--- openaddressing.py
+++ openaddressing.py
@@ -1,1 +1,29 @@
 # open addressing hash table implementation
+
+__author__ = 'devansh.mht@gmail.com'
+
+def hash(value, index):
+  pass
+
+class OpenAddressing:
+  
+  def __init__(self, size):
+    self.size = size
+    self.table = [] * size;
+    self.delete_me  = [] * size
+    
+  def insert(self, i):
+    """inserts i in the hash table"""
+    index = 0
+    while index < self.size:
+      hash_value = hash(i, index)
+      if not self.table[hash_value]:
+        self.table[hash_value] = i
+        return True
+    return False
+  
+  def delete(self, i):
+    pass
+  
+  def search(self, i):
+    pass
