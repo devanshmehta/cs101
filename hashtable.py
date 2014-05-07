a4c08d45a48567624adf16a0d2eb462016b1dd49
Devansh Mehta <devansh.mht@gmail.com>
2014-05-07 07:42:35 -0400

    implementation for search and insert

--- hashtable.py
+++ hashtable.py
@@ -5,6 +5,9 @@ __author__ == 'devansh.mht@gmail.com'
 def hash(i, table_size):
   pass
 
+def insert_at_tail(node):
+  """inserts at the tail"""
+
 class Node:
   
   def __init__(self, data = None, next = None):
@@ -19,11 +22,36 @@ class HashTable:
     
   def insert(self, i):
     hash_value = hash(i, size)
-    values = self.table[hash_value]
-  
+    head = self.table[hash_value]
+    #insert at tail
+    if not head:
+      node = Node(i, None)
+      self.table[hash_value] = node
+      return True
+    else:
+      # if the data is at head
+      if head.data == i:
+        return False
+      prev_node = head
+      current_node = head.next
+      while current_node:
+        if current_node.data == i:
+          return False
+        prev_node = current_node
+        current_node = current_node.next
+      node = Node(i, None)
+      prev_node.next = node
+      return True
+      
   def search(self, i):
     hash_value = hash(i, size)
-    values = self.table(i, size)
+    head = self.table(i, size)
+    tmp = head
+    while tmp:
+      if tmp.data == i:
+        return True
+      tmp = tmp.next
+    return False
   
   def delete(self, i):
     pass
