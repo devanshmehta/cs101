3142f0d6ce890c9f6f3d2ea071edcd47d1606984
Devansh Mehta <devansh.mht@gmail.com>
2014-05-07 07:05:46 -0400

    initial implementation for insert and search

--- hashtable.py
+++ hashtable.py
@@ -1,3 +1,30 @@
 #hash table with chaining
 
 __author__ == 'devansh.mht@gmail.com'
+
+def hash(i, table_size):
+  pass
+
+class Node:
+  
+  def __init__(self, data = None, next = None):
+    self.data = data
+    self.next = next
+
+class HashTable:
+  
+  def __init__(self, size):
+    self.size = size
+    self.table = [None] * size
+    
+  def insert(self, i):
+    hash_value = hash(i, size)
+    values = self.table[hash_value]
+  
+  def search(self, i):
+    hash_value = hash(i, size)
+    values = self.table(i, size)
+  
+  def delete(self, i):
+    pass
+    
