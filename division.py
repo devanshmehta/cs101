68b11d69da699aa1a00d69b766449d91465c8ed6
Devansh Mehta <devansh.mht@gmail.com>
2014-07-19 13:29:53 -0400

    implementation for slow division and error check

--- division.py
+++ division.py
@@ -1,5 +1,7 @@
 # algorithm to divide the number which runs in O(log(x)) 
 def division(x, y):
+  if y == 0:
+    raise ValueError(str(y) + " divisor passed")
   x, y = abs(x), y = abs(y)
   start = 0
   end = x
@@ -13,3 +15,13 @@ def division(x, y):
     else:
       return mid
   returns start
+  
+def slow_division(x, y):
+  if y == 0:
+    raise ValueError(str(y) + " divisor passed")
+  x, y = abs(x), abs(y)
+  count = 0
+  while x > y:
+    x -= y
+    count += 1
+  return count
