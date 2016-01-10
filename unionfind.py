c1d12b36e7ff7b660d4aeb349a78afbdb08f55ab
Devansh Mehta <devansh.mht@gmail.com>
2016-01-10 12:38:43 -0500

    union find datastructure

---
+++ unionfind.py
@@ -0,0 +1,31 @@
+# This module contains two implementations of union find data structure
+# 1) LinkedList and 2) forest
+
+__author__ = 'devansh.mht@gmail.com'
+
+class LinkedList: 
+
+    '''LinkedList implementation for union disjoint'''
+
+    def __init__(self):
+        pass
+
+    def find(self, x):
+        pass
+    
+    def union(self, x, y):
+        pass
+
+
+class Forest:
+    
+    '''Forest implementation of union disjoint'''
+
+    def __init__(self):
+        pass
+    
+    def find(self, x):
+        pass
+
+    def union(self, x, y):
+        pass
