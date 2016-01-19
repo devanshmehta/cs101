a09e67410dbc8452f8c49a255cb38542cbdd1752
Devansh Mehta <devansh.mht@gmail.com>
2016-01-18 23:03:38 -0500

    forest implementation

--- unionfind.py
+++ unionfind.py
@@ -44,6 +44,39 @@ class LinkedList:
             leaderX.add(i.data)
             self.members[i.data] = leaderX
 
+class Forest:
+    
+    '''Forest implementation of union disjoint'''
+
+    def __init__(self):
+        self.members = {}
+
+    def make_set(self, data):
+        if data in self.members:
+            raise ValueError("member {) already present".format(data))
+        self.members[data] = linkedlist.Node(data)
+
+    def _find(self, x):
+        if x not in self.members:
+            return None
+        node = self.members[x]
+        tmp = node
+        while tmp.prev is not None:
+            tmp = tmp.prev
+        return tmp
+    
+    def find(self, x):
+        ret = self._find(x)
+        if ret is None: return None
+        else: return ret.data
+
+    def union(self, x, y):
+        leaderX = self._find(x)
+        leaderY = self._find(y)
+        if leaderX is None or leaderY is None:
+            return
+        leaderY.prev = leaderX
+
 if __name__ == '__main__':
    uf = LinkedList()
    for i in xrange(100):
@@ -54,16 +87,13 @@ if __name__ == '__main__':
    uf.union(2, 8)
    uf.union(3, 8)
    print uf.find(8)
+   print "*" * 80
+   uf = Forest()
+   for i in xrange(100):
+       uf.make_set(i)
 
-class Forest:
-    
-    '''Forest implementation of union disjoint'''
-
-    def __init__(self):
-        pass
-    
-    def find(self, x):
-        pass
-
-    def union(self, x, y):
-        pass
+   uf.union(1, 3)
+   print uf.find(3)
+   uf.union(2, 8)
+   uf.union(3, 8)
+   print uf.find(8)
